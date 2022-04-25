742effc151c865cb7dd7559f16830f4e94dbb5df
Segun Ajibola <ajbl.segun@gmail.com>
2022-04-25 21:50:43 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      {/* <About />
+      <Skills />
+      <Work />
+      <Contact /> */}
+    </div>
+  );
+}
+
+export default App;
