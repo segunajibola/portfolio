3790ba4798764daa501f3b09bea5a715bed3f6aa
Segun Ajibola <ajbl.segun@gmail.com>
2022-04-29 22:39:48 +0100

    fix skills

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -4,7 +4,6 @@ import HTML from '../assets/html.png';
 import CSS from '../assets/css.png';
 import JavaScript from '../assets/javascript.png';
 import ReactImg from '../assets/react.png';
-import Node from '../assets/node.png';
 import Firebase from '../assets/firebase.png';
 import Git from '../assets/git.png';
 import GitHub from '../assets/github.png';
@@ -14,17 +13,17 @@ import Vercel from '../assets/vercel.png';
 
 const Skills = () => {
   return (
-    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+    <div name='skills' className='w-full pt-96 md:pt-0 h-screen bg-[#0a192f] text-gray-300'>
       {/* Container */}
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-          <div className='absolute md:mb-40 md:pb-48 mb-60 -mt-40 pb-60 pl-4'>
+          <div className='absolute md:mb-32 md:pb-48 mb-96 -mt-10 pb-96 pl-4'>
               <p className='opacity-10 text-8xl font-bold inline border-b-4 border-yellow-600'>Skills</p>
           </div>
 
-          <div className='w-full z-10 grid grid-cols-2 sm:grid-cols-5 gap-2 text-center'>
+          <div className='w-full grid grid-cols-2 sm:grid-cols-5 gap-5 text-center py-10'>
               <div className='rounded-lg shadow-xl shadow-yellow-500 hover:scale-110 hover:shadow-xl duration-500'>
                   <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
-                  <p className='my-4'>HTfML</p>
+                  <p className='my-4'>HTML</p>
               </div>
               <div className='rounded-lg shadow-xl shadow-yellow-500 hover:scale-110 hover:shadow-xl duration-500'>
                   <img className='w-20 mx-auto' src={CSS} alt="HTML icon" />
@@ -51,7 +50,7 @@ const Skills = () => {
                   <p className='my-4'>GIT</p>
               </div>
               <div className='rounded-lg shadow-xl shadow-yellow-500 hover:scale-110 hover:shadow-xl duration-500'>
-                  <img className='w-20 mx-auto' src={Node} alt="HTML icon" />
+                  <img className='w-20 mx-auto' src={GitHub} alt="HTML icon" />
                   <p className='my-4'>GITHUB</p>
               </div>
               <div className='rounded-lg shadow-xl shadow-yellow-500 hover:scale-110 hover:shadow-xl duration-500'>
