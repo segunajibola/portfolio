686337050aa18fa57373b4dae86e9bf2f1019ef2
Segun Ajibola <ajbl.segun@gmail.com>
2022-05-01 21:55:39 +0100

    fix skills

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -98,37 +98,6 @@ const Projects = () => {
           
 
         </div>
-{/* Container */}
-        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-
-            {/* Grid Item */}
-          <div
-            style={{ backgroundImage: `url(${projectsImg})` }}
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
-          >
-            {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                React JS Application
-              </span>
-              <div className='pt-8 text-center'>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Demo
-                  </button>
-                </a>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          
-            {/* Grid Item */}
-          
-        </div>
       </div>
     </div>
   );
