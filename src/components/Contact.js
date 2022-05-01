5bde7165366690f969a38db7d2b953a53df55c6a
Segun Ajibola <ajbl.segun@gmail.com>
2022-05-01 23:07:46 +0100

    fix skills

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div name='contact' className='w-full bg-[#0a192f] flex justify-center items-center p-4'>
+    <div name='contact' className='w-full bg-[#0a192f] flex flex-col justify-center items-center p-4'>
+        <div className='relative pt-20 pl-4'>
+            <p className='absolute opacity-10 text-9xl font-bold inline border-b-4 border-yellow-600'>Contact</p>
+            <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email - myemail@gmail.com</p>
+        </div>
         <form method='POST' action="https://getform.io/f/a699a1b2-f225-434e-b317-1fbbde8e006c" className='flex flex-col max-w-[600px] w-full'>
-            <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
-                <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email - myemail@gmail.com</p>
-            </div>
             <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
             <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
             <textarea className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message'></textarea>
