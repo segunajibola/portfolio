e5eea54b1a39d98d64bd43b253f4a5b83b003354
Segun Ajibola <ajbl.segun@gmail.com>
2022-05-02 09:02:16 +0100

    fix skills

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -23,7 +23,7 @@ const Contact = () => {
               <input className='bg-[#ccd6f6] rounded-lg p-2' type="text" placeholder='Name' name='name' />
               <input className='my-4 p-2 bg-[#ccd6f6] rounded-lg' type="email" placeholder='Email' name='email' />
               <textarea className='bg-[#ccd6f6] rounded-lg p-2' name="message" rows="10" placeholder='Message'></textarea>
-              <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
+              <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Send mail</button>
           </form>
         </div>
       </div>
