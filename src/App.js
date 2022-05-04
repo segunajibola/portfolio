1ab55a7eb8eeb910ba8c7ce821fc8836e0a79b59
Segun Ajibola <ajbl.segun@gmail.com>
2022-05-04 23:33:51 +0100

    used swiper

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Testimonials from "./components/Testimonials";
-import Testi from "./components/Testi";
+import Testi from "./components/Testimonials";
 
 function App() {
   return (
@@ -16,7 +16,6 @@ function App() {
       <About />
       <Tools />
       <Projects />
-      <Testi />
       <Testimonials />
       <Contact />
       <Footer />
