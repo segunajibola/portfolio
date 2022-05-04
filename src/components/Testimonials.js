9f1c659afd17136d94421e6ff69fd2648a3c66af
Segun Ajibola <ajbl.segun@gmail.com>
2022-05-05 00:47:12 +0100

    used swiper

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -13,7 +13,7 @@ import { Autoplay, Pagination, Navigation } from "swiper";
 
 const Testimonials = () => {
   return (
-    <div name='testimonials' className='w-full text-gray-300 bg-[#0a192f]'>
+    <div name='testimonials' className='w-full text-gray-300 overflow-hidden bg-[#0a192f]'>
       <div className='flex flex-col justify-center items-center w-full h-full pt-20'>
         {/* <div className='relative pt-20 pl-4'>
             <p className='opacity-10 text-9xl font-bold inline border-b-4 border-yellow-600'>Contact</p>
@@ -21,7 +21,7 @@ const Testimonials = () => {
         <div className='max-w-[1000px] w-full'>
           <div className='absolute pb-8 pl-4'> 
           {/* absolute -mt-16 pb-8 pl-4 --- relative pt-20 pl-4*/}
-            <p className='text-8xl font-bold inline tracking-tighter opacity-10 border-b-4 border-yellow-600'>
+            <p className='text-[5.8rem] md:text-8xl font-bold inline tracking-tighter opacity-10 border-b-4 border-yellow-600'>
               Testimonials
             </p>
           </div>
@@ -30,61 +30,64 @@ const Testimonials = () => {
           <div className='sm:text-right mt-16 md:mt-18 md:ml-10 text-2xl font-bold md:text-left'>
             <p>What others are saying.</p>
           </div>
-          <Swiper
-            spaceBetween={30}
-            centeredSlides={true}
-            autoplay={{
-              delay: 2500,
-              disableOnInteraction: false,
-            }}
-            pagination={{
-              clickable: true,
-            }}
-            navigation={true}
-            modules={[Autoplay, Pagination, Navigation]}
-            className="w-[34rem] md:w-[38rem] ml-96"
-          >
-            <SwiperSlide className="md:mt-1 p-2">
-              <figure className="bg-gray-800 grid rounded-xl p-5 shadow-lg shadow-gray-700">
-                <img className="w-32 h-32 rounded-full mx-auto" src={shegzy} alt="" />
-                <div className="pt-6 text-center space-y-3 text-gray-500">
-                  <blockquote className='mx-auto'>
-                      <p className="text-lg font-semibold text-gray-300 mx-4">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum adipisci maxime eligendi tempora tempore, consequuntur nihil, libero tenetur sed nesciunt, unde accusantium! Ea sint saepe quaerat quia est eius fugit.
-                      </p>
-                  </blockquote>
-                  <figcaption className="font-medium">
-                      <div className="">
-                          John hff
-                      </div>
-                      <div className="">
-                            Staff Engineer, ABL
-                      </div>
-                  </figcaption>
-                </div>
-              </figure>
-            </SwiperSlide>
-            <SwiperSlide className="md:mt-1 p-2">
-              <figure className="bg-gray-800 grid rounded-xl p-5 shadow-lg shadow-gray-700">
-                <img className="w-32 h-32 rounded-full mx-auto" src={shegzy} alt="" />
-                <div className="pt-6 text-center space-y-3 text-gray-500">
-                  <blockquote className='mx-auto'>
-                      <p className="text-lg font-semibold text-gray-300 mx-4">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum adipisci maxime eligendi tempora tempore, consequuntur nihil, libero tenetur sed nesciunt, unde accusantium! Ea sint saepe quaerat quia est eius fugit.
-                      </p>
-                  </blockquote>
-                  <figcaption className="font-medium">
-                      <div className="">
-                          John Doe
-                      </div>
-                      <div className="">
-                            Staff Engineer, ABL
-                      </div>
-                  </figcaption>
-                </div>
-              </figure>
-            </SwiperSlide>  
-          </Swiper>
+          <div className='md:ml-96'>          
+            <Swiper
+              spaceBetween={30}
+              centeredSlides={true}
+              autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+              }}
+              pagination={{
+                clickable: true,
+              }}
+              navigation={true}
+              modules={[Autoplay, Pagination, Navigation]}
+              className="w-[30rem] md:w-[38rem] grid-cols-end"
+            >
+              <SwiperSlide className="md:mt-1 p-2">
+                <figure className="bg-gray-800 grid rounded-xl p-5 shadow-lg shadow-gray-700">
+                  <img className="w-32 h-32 rounded-full mx-auto" src={shegzy} alt="" />
+                  <div className="pt-6 text-center space-y-3 text-gray-500">
+                    <blockquote className='mx-auto'>
+                        <p className="text-lg font-semibold text-gray-300 mx-4">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum adipisci maxime eligendi tempora tempore, consequuntur nihil, libero tenetur sed nesciunt, unde accusantium! Ea sint saepe quaerat quia est eius fugit.
+                        </p>
+                    </blockquote>
+                    <figcaption className="font-medium">
+                        <div className="">
+                            John hff
+                        </div>
+                        <div className="">
+                              Staff Engineer, ABL
+                        </div>
+                    </figcaption>
+                  </div>
+                </figure>
+              </SwiperSlide>
+              <SwiperSlide className="md:mt-1 p-2">
+                <figure className="bg-gray-800 grid rounded-xl p-5 shadow-lg shadow-gray-700">
+                  <img className="w-32 h-32 rounded-full mx-auto" src={shegzy} alt="" />
+                  <div className="pt-6 text-center space-y-3 text-gray-500">
+                    <blockquote className='mx-auto'>
+                        <p className="text-lg font-semibold text-gray-300 mx-4">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum adipisci maxime eligendi tempora tempore, consequuntur nihil, libero tenetur sed nesciunt, unde accusantium! Ea sint saepe quaerat quia est eius fugit.
+                        </p>
+                    </blockquote>
+                    <figcaption className="font-medium">
+                        <div className="">
+                            John Doe
+                        </div>
+                        <div className="">
+                              Staff Engineer, ABL
+                        </div>
+                    </figcaption>
+                  </div>
+                </figure>
+              </SwiperSlide>  
+            </Swiper>
+          </div>
+
           
           
         </div>
