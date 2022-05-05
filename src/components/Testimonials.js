5ee0e066cc3adcebdb715aec235ef95de7fdf0d3
Segun Ajibola <ajbl.segun@gmail.com>
2022-05-05 04:59:40 +0100

    used swiper

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -13,7 +13,7 @@ import { Autoplay, Pagination, Navigation } from "swiper";
 
 const Testimonials = () => {
   return (
-    <div name='testimonials' className='w-full text-gray-300 overflow-hidden bg-[#0a192f]'>
+    <div name='testimonials' className='w-full text-gray-300 overflow-hidden bg-[#0a192f] dark:bg-gray-100 dark:text-gray-800'>
       <div className='flex flex-col justify-center items-center w-full h-full pt-20'>
         {/* <div className='relative pt-20 pl-4'>
             <p className='opacity-10 text-9xl font-bold inline border-b-4 border-yellow-600'>Contact</p>
@@ -21,7 +21,7 @@ const Testimonials = () => {
         <div className='max-w-[1000px] w-full'>
           <div className='absolute pb-8 pl-4'> 
           {/* absolute -mt-16 pb-8 pl-4 --- relative pt-20 pl-4*/}
-            <p className='text-[4.5rem] md:text-8xl font-bold inline tracking-tighter opacity-10 border-b-4 border-yellow-600'>
+            <p className='text-[4.5rem] sm:text-[7rem] md:text-8xl font-bold inline tracking-tighter opacity-10 border-b-4 border-yellow-600'>
               Testimonials
             </p>
           </div>
@@ -46,15 +46,15 @@ const Testimonials = () => {
               className="w-[24rem] sm:w-[30rem] m-auto md:w-[38rem]"
             >
               <SwiperSlide className="md:mt-1 p-2">
-                <figure className="bg-gray-800 grid rounded-xl p-5 shadow-lg shadow-gray-700">
+                <figure className="bg-gray-800 dark:bg-gray-400 grid rounded-xl p-5 shadow-lg shadow-gray-700">
                   <img className="w-32 h-32 rounded-full mx-auto" src={shegzy} alt="" />
-                  <div className="pt-6 text-center space-y-3 text-gray-500">
+                  <div className="pt-6 text-center space-y-3">
                     <blockquote className='mx-auto'>
-                        <p className="text-lg font-semibold text-gray-300 mx-4">
+                        <p className="text-lg font-semibold text-gray-300 mx-4 dark:text-gray-900">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum adipisci maxime eligendi tempora tempore, consequuntur nihil, libero tenetur sed nesciunt, unde accusantium! Ea sint saepe quaerat quia est eius fugit.
                         </p>
                     </blockquote>
-                    <figcaption className="font-medium">
+                    <figcaption className="font-medium text-gray-500 dark:text-gray-800">
                         <div className="">
                             John hff
                         </div>
@@ -66,15 +66,15 @@ const Testimonials = () => {
                 </figure>
               </SwiperSlide>
               <SwiperSlide className="md:mt-1 p-2">
-                <figure className="bg-gray-800 grid rounded-xl p-5 shadow-lg shadow-gray-700">
+                <figure className="bg-gray-800 dark:bg-gray-400 grid rounded-xl p-5 shadow-lg shadow-gray-700">
                   <img className="w-32 h-32 rounded-full mx-auto" src={shegzy} alt="" />
-                  <div className="pt-6 text-center space-y-3 text-gray-500">
+                  <div className="pt-6 text-center space-y-3">
                     <blockquote className='mx-auto'>
-                        <p className="text-lg font-semibold text-gray-300 mx-4">
+                        <p className="text-lg font-semibold text-gray-300 dark:text-gray-900 mx-4">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum adipisci maxime eligendi tempora tempore, consequuntur nihil, libero tenetur sed nesciunt, unde accusantium! Ea sint saepe quaerat quia est eius fugit.
                         </p>
                     </blockquote>
-                    <figcaption className="font-medium">
+                    <figcaption className="font-medium text-gray-500 dark:text-gray-800">
                         <div className="">
                             John Doe
                         </div>
--- src/components/Tools.js
+++ src/components/Tools.js
@@ -11,51 +11,51 @@ import Vercel from '../assets/vercel.png';
 
 const Tools = () => {
   return (
-    <div name='tools' className='w-full pt-32 md:pt-0 md:h-screen bg-[#0a192f] text-gray-300'>
+    <div name='tools' className='w-full pt-32 md:pt-0 md:h-screen bg-[#0a192f] dark:bg-gray-100 text-gray-300 dark:text-gray-800'>
       {/* Container */}
       <div className='max-w-[950px] mx-auto p-4 flex flex-col justify-center h-full'>
-          <div className='absolute md:mb-32 md:pb-48 mb-96 -mt-10 pb-96 pl-4'>
+          <div className='absolute md:mb-32 md:pb-48 mb-96 sm:pb-2 -mt-10 pb-96 pl-4'>
               <p className='opacity-10 text-9xl font-bold inline border-b-4 border-yellow-600'>Tools</p>
           </div>
 
           <div className='w-full z-10 grid grid-cols-2 sm:grid-cols-5 gap-5 text-center py-10'>
-              <div className='rounded-lg shadow-xl shadow-yellow-500 hover:scale-110 hover:shadow-xl duration-500'>
+              <div className='rounded-lg shadow-xl shadow-gray-600 hover:scale-110 hover:shadow-xl duration-500'>
                   <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
                   <p className='my-4'>HTML</p>
               </div>
-              <div className='rounded-lg shadow-xl shadow-yellow-500 hover:scale-110 hover:shadow-xl duration-500'>
+              <div className='rounded-lg shadow-xl shadow-gray-600 hover:scale-110 hover:shadow-xl duration-500'>
                   <img className='w-20 mx-auto' src={CSS} alt="HTML icon" />
                   <p className='my-4'>CSS</p>
               </div>
-              <div className='rounded-lg shadow-xl shadow-yellow-500 hover:scale-110 hover:shadow-xl duration-500'>
+              <div className='rounded-lg shadow-xl shadow-gray-600 hover:scale-110 hover:shadow-xl duration-500'>
                   <img className='w-20 mx-auto' src={JavaScript} alt="HTML icon" />
                   <p className='my-4'>JAVASCRIPT</p>
               </div>
-              <div className='rounded-lg shadow-xl shadow-yellow-500 hover:scale-110 hover:shadow-xl duration-500'>
+              <div className='rounded-lg shadow-xl shadow-gray-600 hover:scale-110 hover:shadow-xl duration-500'>
                   <img className='w-20 mx-auto' src={Bootstrap} alt="HTML icon" />
                   <p className='my-4'>BOOTSTRAP</p>
               </div>
-              <div className='rounded-lg shadow-xl shadow-yellow-500 hover:scale-110 hover:shadow-xl duration-500'>
+              <div className='rounded-lg shadow-xl shadow-gray-600 hover:scale-110 hover:shadow-xl duration-500'>
                   <img className='w-20 mx-auto' src={Tailwind} alt="HTML icon" />
                   <p className='my-4'>TAILWINDCSS</p>
               </div>
-              <div className='rounded-lg shadow-xl shadow-yellow-500 hover:scale-110 hover:shadow-xl duration-500'>
+              <div className='rounded-lg shadow-xl shadow-gray-600 hover:scale-110 hover:shadow-xl duration-500'>
                   <img className='w-20 mx-auto' src={ReactImg} alt="HTML icon" />
                   <p className='my-4'>REACT</p>
               </div>
-              <div className='rounded-lg shadow-xl shadow-yellow-500 hover:scale-110 hover:shadow-xl duration-500'>
+              <div className='rounded-lg shadow-xl shadow-gray-600 hover:scale-110 hover:shadow-xl duration-500'>
                   <img className='w-20 mx-auto' src={Git} alt="HTML icon" />
                   <p className='my-4'>GIT</p>
               </div>
-              <div className='rounded-lg shadow-xl shadow-yellow-500 hover:scale-110 hover:shadow-xl duration-500'>
-                  <img className='w-20 mx-auto' src={GitHub} alt="HTML icon" />
+              <div className='rounded-lg shadow-xl shadow-gray-600 hover:scale-110 hover:shadow-xl duration-500'>
+                  <img className='w-20 mx-auto dark:bg-gray-800' src={GitHub} alt="HTML icon" />
                   <p className='my-4'>GITHUB</p>
               </div>
-              <div className='rounded-lg shadow-xl shadow-yellow-500 hover:scale-110 hover:shadow-xl duration-500'>
+              <div className='rounded-lg shadow-xl shadow-gray-600 hover:scale-110 hover:shadow-xl duration-500'>
                   <img className='w-20 mx-auto' src={Firebase} alt="HTML icon" />
                   <p className='my-4'>FIREBASE</p>
               </div>
-              <div className='rounded-lg shadow-xl shadow-yellow-500 hover:scale-110 hover:shadow-xl duration-500'>
+              <div className='rounded-lg shadow-xl shadow-gray-600 hover:scale-110 hover:shadow-xl duration-500'>
                   <img className='w-20 mx-auto' src={Vercel} alt="HTML icon" />
                   <p className='my-4'>VERCEL</p>
               </div>
