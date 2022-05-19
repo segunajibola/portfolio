d5ee0fa9c5340424c81659d64b1cc7473845516a
Segun Ajibola <ajbl.segun@gmail.com>
2022-05-19 09:59:38 +0100

    changeVercel

--- src/components/About.js
+++ src/components/About.js
@@ -14,10 +14,16 @@ const About = () => {
             <p>A little bit about me.</p>
           </div>
           <div>
-            <p>I love building awesome software that improves
-            the lives & business of others. I create responsive websites
-            for clients ranging from portfolio webpages for individuals and functional websites for small-businesses. I also love design and teaching.</p>
-            <p className='mt-3'>In my leisures, I read about Arsenal, tech news and latest gadgets. PS. I use <a href='https://www.twitter.com/iamsegunajibola' className='text-blue-400'>Twitter</a> alot.</p>  
+            <p>I love building cool software that improves
+            the lives & business of others.</p>
+            
+            <p>I create responsive websites from scratch for creatives ranging from portfolio websites to functional websites for small businesses so that they can focus on what makes them money.</p>
+            
+            <p>I'm intrested in design, writing, teaching, tech, and how small technical things works.</p>
+
+            <p className='mt-3'>In my free time, I read about Arsenal, tech news and latest gadgets.</p>
+            
+            <p>PS: I use <a href='https://www.twitter.com/iamsegunajibola' className='text-blue-400'>Twitter</a> alot.</p>  
           </div>
         </div>
       </div>
