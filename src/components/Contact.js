bf310f887a7ee159a82af55ef040c11d3bc0ef0d
Segun Ajibola <ajbl.segun@gmail.com>
2022-05-21 03:37:19 +0100

    fixSocialLinks

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -13,11 +13,48 @@ const Contact = () => {
           <div className='sm:text-right mt-20 text-2xl font-bold'>
             <p>Submit this form to send me an email.</p>
           </div>
-          <form method='POST' action="" className='relative flex flex-col max-w-[600px] w-full -mt-5 sm:mt-20 md:mt-16'>
-              <input className='bg-gray-400 dark:bg-gray-300 text-gray-900 rounded-lg p-2 outline-none placeholder:text-gray-200' type="text" placeholder='Name' name='name' />
-              <input className='bg-gray-400 dark:bg-gray-300 my-4 p-2 text-gray-800 rounded-lg outline-none placeholder:text-gray-200' type="email" placeholder='Email' name='email' />
-              <textarea className='bg-gray-400 dark:bg-gray-300 text-gray-800 rounded-lg p-2 outline-none placeholder:text-gray-200' name="message" rows="10" placeholder='Message'></textarea>
-              <button className='bg-gray-400 dark:bg-gray-300 text-gray-100 border-2 hover:text-gray-800 hover:bg-gray-100 hover:border-gray-200 px-4 py-3 my-8 mx-auto flex items-center dark:text-gray-900 dark:border-gray-500 dark:hover:bg-gray-300 dark:hover:border-gray-800'>Send mail</button>
+          <form method="POST" action="https://formsubmit.co/cfd3b6bc430690ebff065253cdfdb7b5" className='relative flex flex-col max-w-[600px] w-full -mt-5 sm:mt-20 md:mt-16'>
+              <input
+                className='bg-gray-400 dark:bg-gray-300 text-gray-900 rounded-lg p-2 outline-none placeholder:text-gray-200'
+                type="text"
+                placeholder='Name'
+                name='name'
+                required
+              />
+              <input
+                className='bg-gray-400 dark:bg-gray-300 my-4 p-2 text-gray-800 rounded-lg outline-none placeholder:text-gray-200'
+                type="email"
+                placeholder='Email'
+                name='email'
+                required
+              />
+              <input 
+                type="text"
+                name="subject"
+                placeholder="Subject"
+                className="bg-gray-400 dark:bg-gray-300 mb-4 p-2 text-gray-800 rounded-lg outline-none placeholder:text-gray-200"
+                required
+              />
+              <textarea
+                className='bg-gray-400 dark:bg-gray-300 text-gray-800 rounded-lg p-2 outline-none placeholder:text-gray-200'
+                name="message"
+                rows="10"
+                placeholder='Message'>
+              </textarea>
+              <input 
+                type="hidden"
+                name="_next"
+                value="https://segunajibola.com/#contact"
+              />
+              <input
+                type="hidden"
+                name="_captcha"
+                value="false"
+              />
+              <button 
+                className='bg-gray-400 dark:bg-gray-300 text-gray-100 border-2 hover:text-gray-800 hover:bg-gray-100 hover:border-gray-200 px-4 py-3 my-8 mx-auto flex items-center dark:text-gray-900 dark:border-gray-500 dark:hover:bg-gray-300 dark:hover:border-gray-800'>
+                  Send mail
+              </button>
           </form>
         </div>
       </div>
