d90c1bf7aa0b75f11c1aa5efb4ac12c179bf8f12
Segun Ajibola <ajbl.segun@gmail.com>
2023-02-13 02:13:47 +0100

    switchToVite

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
