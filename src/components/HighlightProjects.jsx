ccfa7ea5143be1f42e597a14507d656497b48cd7
Segun Ajibola <ajbl.segun@gmail.com>
2023-04-08 17:05:00 +0100

    addPadding

--- src/components/HighlightProjects.jsx
+++ src/components/HighlightProjects.jsx
@@ -12,7 +12,7 @@ const HighlightProjects = () => {
             Projects
           </p>
         </div>
-        <div className='grid grid-cols-1 pt-20 md:-mt-5 md:pt-32 md:grid-cols-2 lg:grid-cols-3 gap-6 z-10'>
+        <div className='grid grid-cols-1 pt-20 md:-mt-5 md:pt-32 md:grid-cols-2 lg:grid-cols-3 gap-6 z-10 px-6'>
           {data.filter((item, index) => index < 6).map((project) => (
            <ProjectCard project={project} />
           ))}
